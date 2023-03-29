45573d400e912820814e393d603a754a19bcb0c4
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-03-29 14:48:40 +0530

    :bug: fixed clap issue

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value_t = 1)]
     problem_number: u8,
 }
 
