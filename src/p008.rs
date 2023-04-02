034424c3fe9ebc8e38fa899ff73f6e24782ae544
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-04-02 23:54:16 +0530

    :sparkles: solve p8

--- src/p008.rs
+++ src/p008.rs
@@ -1,6 +1,27 @@
 pub fn run() {
     let mut line = String::new();
-    println!("Enter :");
+    println!("Enter input: ");
     std::io::stdin().read_line(&mut line).unwrap();
-    println!("Hello , {}", line);
+
+    let mut max_prod: u64 = 0;
+
+    for i in 0..(line.len() - 13) {
+        let mut product: u64 = 1;
+
+        for j in 0..13 {
+            let num: u64 = line
+                .chars()
+                .nth(i + j)
+                .unwrap()
+                .to_digit(10)
+                .unwrap()
+                .into();
+
+            product *= num;
+        }
+
+        max_prod = u64::max(max_prod, product.into());
+    }
+
+    print!("{}", max_prod);
 }
